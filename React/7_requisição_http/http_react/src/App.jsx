6362b7bad03e98f3eebb8e89608d40a02a180b4f
Diegooaraujo <149812280+Diegooaraujo@users.noreply.github.com>
2024-12-03 18:02:24 -0300

    .

--- React/7_requisição_http/http_react/src/App.jsx
+++ React/7_requisição_http/http_react/src/App.jsx
@@ -1,13 +1,29 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+
+const url = "http://localhost:3000/products"
 
 import './App.css'
 
 function App() {
-  
+  //resgatando dados
+  const [products,setProducts]=useState([])
+  useEffect(()=>{
+    async function getData(){
+      const res = await fetch(url)
+      const data = await res.json()
+
+      setProducts(data)
+    }
+    getData()
+  },[])
 
   return (
     <>
-  
+      <ul>
+        {products.map((product)=>(
+          <li key={product.id}> {product.name} - {product.price}</li>
+        ))}
+      </ul>
     </>
   )
 }
