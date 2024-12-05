b68ac565ea664ce06e5211b12ce976023690db89
Diegooaraujo <149812280+Diegooaraujo@users.noreply.github.com>
2024-12-05 18:55:31 -0300

    router

---
+++ React/8_Router/router/src/main.jsx
@@ -0,0 +1,29 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+import Contact from './router/contact.jsx'
+
+
+
+import{
+  createBrowserRouter, RouterProvider, Router 
+} from "react-router-dom"
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element:<App/>
+  },
+  {
+    path:"contact",
+    element:<Contact/>
+  }
+])
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router}/>
+  </StrictMode>,
+)
